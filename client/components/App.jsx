f3335d01d22e365ff7b89a19e7fa89c2d2648338
Quy Nguyen <imnnquy@gmail.com>
2018-04-14 18:03:40 +0800

    Add time

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,21 +8,27 @@ export default class App extends React.Component {
 
   getGreeting(user) {
     if (user) {
-      return <h1 style={{
+      return (<h1 style={{
           color: '#0F0'
-        }}>Hello, {this.formatName(user)}!</h1>;
+        }}>Hello, {this.formatName(user)}!</h1>)
     }
-    return <h1 style={{
+    return (<h1 style={{
         color: '#F00'
-      }}>Hello, Stranger.</h1>;
+      }}>Hello, Stranger.</h1>)
   };
 
   render() {
-    const user = {firstName: 'Peter', lastName: 'Pan'};
+    const user = {
+      firstName: 'Eric',
+      lastName: 'Nguyen'
+    };
+    const timeNotify = <h2>
+      It is {new Date().toLocaleTimeString()}.</h2>
     const element = this.getGreeting(user);
 
     return (<div>
       {element}
+      {timeNotify}
     </div>);
   }
 }
